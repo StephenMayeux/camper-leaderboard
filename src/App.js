5f614d8051fedcdf849f8625e1a7e71237b81e19
Stephen Mayeux <stephenmayeux@gmail.com>
2017-08-14 17:33:02 -0500

    repeal and replace xmlhttprequest

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
 
-  state = {
-    top100: undefined
+    this.state = {
+      top100: null
+    }
+
   }
 
-  //Get FCC data 
-getFCCData(url) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    //  console.log(JSON.parse(xhttp.response));
-     this.setState({top100: JSON.parse(xhttp.response)});
-     console.log(this.state.top100);
-    }
-  };
-  //Set up for the request (directions)
-  xhttp.open("GET", url, true);
-  //Request fires
-  xhttp.send();
-};
-
-
-componentDidMount(){
-this.getFCCData("https://fcctop100.herokuapp.com/api/fccusers/top/recent");
-}
-
+  componentDidMount() {
+    axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+      .then(({ data }) => {
+        this.setState({ top100: data })
+      })
+  }
 
   render() {
     return (
       <div className="App">
-      <table>
-        <caption>Leaderboard</caption>
-        <tr>
-          <th>#</th>
-           <th>Camper Name</th>
+        <table>
+          <caption>Leaderboard</caption>
+          <tr>
+            <th>#</th>
+            <th>Camper Name</th>
             <th>Points in past 30 days</th>
-             <th>All Time Points</th>
-        </tr>
-        <tbody>
-            <tr>
-
-        </tr>
+            <th>All Time Points</th>
+          </tr>
+          <tbody>
+            <tr></tr>
           </tbody>
-      
-      </table>
+        </table>
       </div>
     );
   }
